ea019e46db05a55da00918066d794a60aa884a38
lxxccl <1714140986@qq.com>
2022-08-02 14:26:47 +0800

    first

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  productionSourceMap: false,
+  // 关闭ESLINT校验工具
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production' ? '/my-project/' : '/',
+  //配置代理跨域
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://gmall-h5-api.atguigu.cn',
+        changeOrigin: true
+      }
+    }
+  }
+}
